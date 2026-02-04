4049dc2354278d13a019b7292e12196b9b236608
AndresGar1217 <731366@student.dusd.net>
2026-02-04 11:45:09 -0800

    finished car
    working on plane

--- P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
 
         //Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
