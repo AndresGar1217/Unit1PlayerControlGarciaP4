405aa5b26a862df371940b570fe03f5f35a3cfdb
AndresGar1217 <731366@student.dusd.net>
2026-02-10 11:04:49 -0800

    finished the entire control project

--- P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
+++ P4PlayerControlGarcia/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera; 
+    public Camera otherCamera;
+    public KeyCode switchKey;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +25,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey)) 
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            otherCamera.enabled = !otherCamera.enabled;
+        }
     }
 }
